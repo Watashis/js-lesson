e2cff8bdba04693428dbb4cd834c95db89662bb5
Watashi <Vlad73RU86@yandex.ru>
2019-01-14 19:07:12 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+from flask import Flask, render_template, request, send_file
 
+app = Flask(__name__, static_url_path='')
+
+@app.route('/')
+def index():
+    return render_template('index.html', title='Home')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5001, debug=True)
