ce90369a6967ce81a4f46b049bfa12bab5b43b2d
Watashi <Vlad73RU86@yandex.ru>
2019-01-15 17:51:25 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, send_file
+import requests
 
 app = Flask(__name__, static_url_path='')
 
@@ -6,5 +7,11 @@ app = Flask(__name__, static_url_path='')
 def index():
     return render_template('index.html', title='Home')
 
+@app.route('/test')
+def test():
+    video = requests.get('https://vjs.zencdn.net/v/oceans.mp4', stream=True).raw)
+    video = io.BytesIO(video)
+    return send_file(video, mimetype='video/mp4')
+    
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5001, debug=True)
